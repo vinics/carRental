3f65674af14b7c084be65c83f3ddca5e813fa700
vinics <vinics.dr@gmail.com>
2022-10-11 19:18:39 -0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import express 
-from 'express'
+import 'reflect-metadata';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-const app = express()
-const PORT = process.env.PORT || 3333
+import './database';
+import './shared/container';
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello There!!!' })
-})
+import { router } from './routes';
+import swaggerFile from './swagger.json';
 
-app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+const app = express();
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use(router);
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
